fc81f6c3890a3587d985f19683e6ebf20af52504
Nikita Proskurin <hemerdinger@mail.ru>
2025-05-04 22:53:22 +0300

    вывел масив с карточками на страницу и реализовал удаление через колбэк функцию

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardNumber, delateItem) {
+  const card = cardTemplate.querySelector('.places__item').cloneNode(true);
+  card.querySelector('.card__title').textContent = cardNumber.name;
+  card.querySelector('.card__image').src = cardNumber.link;
+  card.querySelector('.card__image').alt = `Пейзах местности ${cardNumber.name}`;
+  card.querySelector('.card__delete-button').addEventListener('click', () => {
+    delateItem(card);
+  });
+  
+  return card
+};
 
 // @todo: Функция удаления карточки
 
+function handleDeleteCard(item) {
+  item.remove();
+};
+
 // @todo: Вывести карточки на страницу
+function renderCard(card) {
+  placesList.append(card)
+};
+
+initialCards.forEach((item) => {
+  renderCard(createCard(item, handleDeleteCard))
+})
