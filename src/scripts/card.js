a6f12bc4a6b18eb7322e4c42048646ab94562ce4
Nikita Proskurin <hemerdinger@mail.ru>
2025-05-26 03:21:03 +0300

    Изменены пути проекта

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,9 +1,6 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
-const placesList = document.querySelector('.places__list');
-
 // @todo: Функция создания карточки
 function createCard(cardNumber, delateItem, openImg, likeButtons) {
   const card = cardTemplate.querySelector('.places__item').cloneNode(true);
@@ -30,18 +27,11 @@ function createCard(cardNumber, delateItem, openImg, likeButtons) {
 
 // @todo: Функция удаления карточки
 
-function handleDeleteCard(item) {
-  item.remove();
-};
+
 
 // @todo: Вывести карточки на страницу
-function renderCard(card) {
-  placesList.append(card)
-};
 
 // @todo: Добавление карточки на страницу
-function addCard(card) {
-  placesList.prepend(card)
-}
 
-export {renderCard, createCard, handleDeleteCard, addCard}
+
+export {createCard}
